7b26d8b64c10599829dfd08a18cd06829ac8d643
Noble Mathew <noblemathew2004@gmail.com>
2023-09-10 04:05:04 -0500

    Connected to api and showed json file on frontend

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
-import { LocalStorage } from "node-localstorage";
 
-global.localStorage = new LocalStorage("./scratch");
+// global.localStorage = new LocalStorage("./scratch");
 
 // Set up Express App for use
 //Line 10-11 set up the CORS policy and the request body parser
@@ -13,13 +12,17 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-// params = {
-//   api_key: '7814cf75a6954dd790bcc59be0e80db7',
-//   queries: obj[queries]
-// }
+let params = {
+  api_key: '7814cf75a6954dd790bcc59be0e80db7',
+ }
 
 // The url using the parameters
-// const api_url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${encodeURIComponent(obj.api_key)}&query=${encodeURIComponent(obj.queries)}&cuisines=${obj.cuisines}&intolerances=${obj.intolerances}&includeIngredients=${obj.ingredients}`
+const api_url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${encodeURIComponent(
+  params.api_key
+)}&findIngredients=${params.ingredients}`;
+
+
+//Additional parameters  query=${encodeURIComponent(obj.queries)}&cuisines=${obj.cuisines}&intolerances=${obj.intolerances}&
 
 // Getting the data from the API using fetch
 
@@ -28,6 +31,37 @@ app.listen(port, () => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("done");
+  try {
+    // Your route handling logic
+    // Use for...of to iterate over object properties
+    for (const [key, value] of Object.entries(req.body)) {
+      params[key] = value;
+    }
+
+    console.log(req.body, params);
+
+    // Use JSON.stringify to convert params to a JSON string
+  }
+      catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  };
+});
+
+
+// Connecting to the 3rd party API
+app.get("/api/data", async (req, res) => {
+  try {
+    // Make a GET request to the third-party API
+    const response = await axios.get(api_url);
+
+    // Extract the relevant data from the response
+    const data = response.data;
+
+    // Send the data as JSON to the frontend
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data from the third-party API:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function returnText() {
   var ingredients = document.getElementById("ingredients").value;
 
   try {
-    const url = "http://localhost:3001/";
+    const url = "http://localhost:3001";
     let options = {
       method: "POST",
       headers: {
@@ -13,19 +13,47 @@ async function returnText() {
       },
       body: JSON.stringify({ /*query , cuisine, intolerences,*/ ingredients }),
     };
-    fetch(url, options).then((res) => {
-      console.log(res);
-    });
+    fetch(url, options).then((res) => { });
   } catch (e) {
     console.error(e);
   }
 }
+function fetchData() {
+  const apiUrl = "http://localhost:3001/api/data"; // Replace with your backend URL
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Parse the JSON response
+    })
+    .then((data) => {
+      // Handle and display the data on the frontend
+      const dataContainer = document.getElementById("data-container");
+      dataContainer.innerHTML = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
 
-const form = document.querySelector("form");
+function inputChanges() {
+  // Get references to the textarea and output text box
+  const textarea = document.getElementById("ingredients");
+  const outputText = document.getElementById("outputText");
+
+  // Add an event listener to the textarea to capture input changes
+  textarea.addEventListener("input", function () {
+    // Get the value of the textarea
+    const inputValue = textarea.value;
+  });
+}
+
+/*const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const fd = new FormData(form);
   const obj = Object.fromEntries(fd);
      console.log(obj);
-});
+});*/
